e58a3d582220eddbf5bdf9e89186d8b45f53912b
dantheman0124 <daniel.bassani@outlook.com>
2022-04-19 19:49:45 -0400

    bug fix and rewrite for many issues

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # num-pad-array
-This npm package provides functionality to pad each element of an array with 0s.
+Provides functionality to pad each element of an array with 0s, according to the number with the most digits in the array.
+
+```
+Example:
+    Input Array: [ 1000, 12345, 3, -17, 54 ]
+    Output Array: [ '01000', '12345', '00003', '-00017', '00054' ]
+
+```
+
+```
+Installation:
+    npm i num-pad-array
+
+```
+
+```
+Usage:
+    var npa = require('num-pad-array');
+
+    let result = npa.pad([ 1000, 12345, 3, -17, 54 ]); // array can be numeric
+    console.log(result); // [ '01000', '12345', '00003', '-00017', '00054' ]
+
+    let result = npa.pad(['5555', '1', '800', '7', '-40506']); // array can be a string representation of numbers
+    console.log(result); // [ '05555', '00001', '00800', '00007', '-40506' ]
+
+```
+
+
+
--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 /**
- * This function will pad an array of Strings with 0s.
- * @param {String} arr An array of String representation of numbers.
- * @returns An array of Strings, where each number is padded to contain the same number of digits.
- * Negative numbers will have an extra "-" sign.
- */
-exports.padString = function(arr) {
-    return padder(arr);
-}
-
-/**
  * This function will pad an array of numbers with 0s. Result will be an array of Strings.
  * @param {Number} arr An array of numbers.
  * @returns An array of Strings, where each number is padded to contain the same number of digits.
  * Negative numbers will have an extra "-" sign.
  */
 exports.pad = function(arr) {
-    return padder(arr.map(String));
+    return padder(arr);
 }
 
 function padder(arr) {
-    let maxLen = 0;
+    arr = arr.map(Number);
+
+    let digits = []
     let isNegative = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].length > maxLen) {
-            maxLen = arr[i].length;
-        }
 
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0) {
-            isNegative.push(true);
+            isNegative[i] = true;
+            arr[i] = Math.abs(arr[i])
         } else {
-            isNegative.push(false);
+            isNegative[i] = false;
         }
     }
 
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; arr[i].length < maxLen; j++) {
-            if (j == 0) {
-                if (arr[i].substring(0, 1) === '-') {
-                    arr[i] = arr[i].substring(1);
-                }
-            } else if (j === maxLen - 1 && isNegative[i]) {
-                arr[i] = '-0' + arr[i];
-            } else {
-                arr[i] = '0' + arr[i];
-            }
+        if (arr[i] === 0 || arr[i] === 1) {
+            digits[i] = arr[i];
+        } else {
+            digits[i] = Math.trunc(Math.log10(arr[i]) + 1);
         }
     }
 
-    return arr;
+    let max = Math.max.apply(Math, digits);
+    let strArr = arr.map(String);
+
+    for(let i = 0; i < strArr.length; i++) {
+        for(let j = 0; strArr[i].length < max; j++) {
+            strArr[i] = '0' + strArr[i];
+        }
+        if (isNegative[i]) {
+            strArr[i] = '-' + strArr[i];
+        }
+    }
+    
+    return strArr;
 }
